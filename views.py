6224c75efe0082398ca2327ebd74728accd7751b
jerem-uzoma <31065914+jerem-uzoma@users.noreply.github.com>
2017-08-16 13:45:34 +0100

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,19 @@
-# add to the top
+
 from collection.forms import ContactForm
 
-# add to your views
 def contact(request):
-    form_class = ContactForm
-    
-    return render(request, 'contact.html', {
-        'form': form_class,
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+
+            print form.cleaned_data['my_form_field_name']
+
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render_to_response('contact.html', {
+        'form': form,
     })
